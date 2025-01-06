a488aacf5bf473385f81cba9a192f5cb0f80a354
endothedev <endothedev@gmail.com>
2025-01-06 23:56:09 +0700

    feat: Enhance model management and layout in app and index components
    - Commented out the previous UButton in app.vue for improved layout clarity.
    - Introduced search functionality and active panel management in index.vue, allowing users to filter models and manage their display.
    - Added loading and error states in index.vue to enhance user feedback during model fetching.
    - Created a new models.vue file to encapsulate model-related features, including search and display logic, improving code organization and maintainability.

---
+++ pages/.gitignore/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+	<BaseLayout>
+		<template #header>
+			<div class="text-xl flex items-center p-5 h-full">
+				<h1>Chat WIP</h1>
+			</div>
+		</template>
+		<template #default>
+			<h1>CHAT</h1>
+		</template>
+		<template #footer>
+			<!--  -->
+		</template>
+	</BaseLayout>
+</template>
