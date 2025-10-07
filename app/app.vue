779e4692d6b2599376d8a8c271a3fd10bb2f6052
endothedev <endothedev@gmail.com>
2025-10-07 19:54:18 +0700

    feat(sidebar): enhance sidebar animations and visibility logic for smoother transitions

--- app/app.vue
+++ app/app.vue
@@ -1,40 +1,54 @@
 <script setup lang="ts">
-const appConfig = useAppConfig();
-const settingsStore = useSettingsStore();
-const isSmallViewport = useMediaQuery('(min-width: 768px)');
-const { fadeTransition, radiusClasses } = useUIUtils();
+  import { ref, watch } from "vue";
+
+  const appConfig = useAppConfig();
+  const settingsStore = useSettingsStore();
+  const isSmallViewport = useMediaQuery("(min-width: 768px)");
+
+  // Ref for sequencing text fade
+  const showLabels = ref(settingsStore.sidebarOpen);
+
+  // Ref for content padding
+  const contentOpen = ref(settingsStore.sidebarOpen);
+
+  // Watcher to sequence animations
+  watch(
+    () => settingsStore.sidebarOpen,
+    (newVal) => {
+      if (newVal) {
+        // Opening: show content and text after delay
+        contentOpen.value = true;
+        setTimeout(() => {
+          showLabels.value = true;
+        }, 300);
+      } else {
+        // Closing: hide text immediately, then hide content
+        showLabels.value = false;
+        setTimeout(() => {
+          contentOpen.value = false;
+        }, 300);
+      }
+    }
+  );
 </script>
 
 <template>
-	<ClientOnly>
-		<UApp :toaster="appConfig.toaster">
-			<div class="flex">
-				<AppSidebar>
-					<template #default>
-						<AppSidebarNav :show-labels="settingsStore.sidebarOpen" />
-					</template>
+  <ClientOnly>
+    <UApp :toaster="appConfig.toaster">
+      <div class="flex">
+        <AppSidebar>
+          <template #default>
+            <AppSidebarNav :show-labels="showLabels" />
+          </template>
+        </AppSidebar>
 
-					<template #footer>
-						<UButton
-							:to="'/settings'"
-							:class="['h-10 flex w-full justify-center', radiusClasses]">
-							<UIcon
-								name="i-heroicons-cog-6-tooth"
-								class="fixed left-4 w-6 h-6" />
-							<Transition v-bind="fadeTransition">
-								<span v-if="settingsStore.sidebarOpen">Settings</span>
-							</Transition>
-						</UButton>
-					</template>
-				</AppSidebar>
-
-				<!-- CONTENT -->
-				<div
-					class="fixed w-full flex justify-center transition-all duration-300 pl-14"
-					:class="[isSmallViewport && settingsStore.sidebarOpen ? 'md:pl-56' : 'md:pl-14']">
-					<NuxtPage />
-				</div>
-			</div>
-		</UApp>
-	</ClientOnly>
+        <!-- CONTENT -->
+        <div
+          class="fixed w-full flex justify-center transition-all duration-300 pl-14"
+          :class="[isSmallViewport && contentOpen ? 'md:pl-56' : 'md:pl-14']">
+          <NuxtPage />
+        </div>
+      </div>
+    </UApp>
+  </ClientOnly>
 </template>
